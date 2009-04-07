ece8f9d6929493b6223b3f3c15fb8ce0cdde6b6a
Matt Madia <mattmadia@gmail.com>
2009-04-07 04:15:17 +0000

    Initial scirpt for gzip-1.3.12.  Has build issues. See : http://ports.haiku-files.org/wiki/app-arch/gzip/1.3.12/2

---
+++ app-arch/gzip/gzip-1.3.12.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+PACKAGE="app-arch/gzip"
+VERSION="1.3.12"
+HOMEPAGE="http://www.gnu.org/software/gzip/"
+URL="http://ftp.gnu.org/gnu/gzip/"
+FILE="gzip-1.3.12.tar.gz"
+PATCH_URL="http://ports.haiku-files.org/export/279/haikuports/trunk/app-arch/gzip/patches/"
+PATCH_FILE="gzip-1.3.12-downstream.patch"
+
+PREFIX=/boot/common
+HAIKUPORTS="http://ports.haiku-files.org/wiki"
+CWD=`pwd`
+
+if [ ! -d ${PACKAGE}/${VERSION} ] ; then
+	mkdir -p ${PACKAGE}/${VERSION}
+fi
+if [ ! -e ${FILE} ] ; then
+	wget ${URL}${FILE}
+fi
+cd ${PACKAGE}/${VERSION}
+tar xvf ${CWD}/${FILE}
+cd gzip-1.3.12
+wget ${PATCH_URL}${PATCH_FILE}
+patch -p1 -i ${PATCH_FILE}
+libtoolize --force --install
+configure --prefix=${PREFIX}
+make && make install && echo  && echo "  Installation complete." || echo "  Installation FAILED !"
+
+echo "  For more information, see ${HAIKUPORTS}/${PACKAGE}"
+echo
