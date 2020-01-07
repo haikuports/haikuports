ad5e917dbb8c30f0a4fc0c01801179a498a3a25c
Gerasim Troeglazov <3dEyes@gmail.com>
2020-01-07 19:22:15 +1000

    KolourPaint: bump version

--- kde-base/kolourpaint/additional-files/run_kbuildsycoca.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-
-# Rebuilds the KService desktop file system configuration cache.
-# --global ignores any user-set files (in XDG_DATA_HOME).
-
-kbuildsycoca5 --global || true
-kbuildsycoca5_x86 --global || true
