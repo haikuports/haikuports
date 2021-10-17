ca777130c3be7993479f3d192c9a8712b4ea979c
Joachim Mairböck <j.mairboeck@gmail.com>
2021-10-17 17:47:50 +0200

    remove the waiting on texlive's post-install scripts because it is not needed

--- app-text/texlive/additional-files/texlive_postinstall.sh
+++ app-text/texlive/additional-files/texlive_postinstall.sh
@@ -3,5 +3,5 @@
 processList=$(ps)
 processCount=$(grep texlive_update.sh <<< "$processList" | wc -l)
 if [ $processCount -eq 0 ]; then # if no texlive_update is running yet, run it
-	/boot/system/bin/texlive_update.sh --wait &
+	/boot/system/bin/texlive_update.sh &
 fi
--- app-text/texlive/additional-files/texlive_update.sh
+++ app-text/texlive/additional-files/texlive_update.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ "$1" == "--wait"]; then
-	sleep 5	# wait for all subpackages to be installed before doing anything
-fi
-
 mktexlsr
 fmtutil-sys --all
 #mtxrun --generate
