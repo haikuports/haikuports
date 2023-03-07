3eee02a6dd87e237ee735c845aa22442fdde6bef
Peter van Dijk <peter@7bits.nl>
2023-03-07 21:54:40 +0100

    also lint changed recipes
    example commit taken from a recent PR:
    
    $ git diff  HEAD HEAD~ --numstat
    7   7   media-sound/freac/{freac-1.1.7.recipe => freac-1.1.6.recipe}
    
    -> lint-new-recipes.sh tries to find `media-sound/freac/{freac-1.1.7.recipe` which does not exist
    
    $ git diff --no-renames HEAD HEAD~ --numstat
    98  0   media-sound/freac/freac-1.1.6.recipe
    0   98  media-sound/freac/freac-1.1.7.recipe
    
    -> lint-new-recipes.sh skips over 1.1.6 (because it is gone) and lints 1.1.7

--- lint-new-recipes.sh
+++ lint-new-recipes.sh
@@ -3,7 +3,7 @@
 git remote add real_origin https://github.com/haikuports/haikuports.git || true
 git fetch real_origin
 
-files=$(git diff $(git rev-parse --abbrev-ref HEAD) real_origin/master --numstat | awk '{print $3}')
+files=$(git diff --no-renames $(git rev-parse --abbrev-ref HEAD) real_origin/master --numstat | awk '{print $3}')
 for file in $files; do
 	if [ -e "$file" ] && [[ "$file" == *recipe ]]; then
 		LINES=$(grep -l '[[:blank:]]$' $file)
