5bab79c95e0bfbe153bafdfda65ac07baf3a530f
Schrijvers Luc <begasus@gmail.com>
2023-06-13 15:36:46 +0200

    cudatext, bump 32bit version (#8853)

--- app-editors/cudatext/additional-files/setup-cudatext.sh
+++ app-editors/cudatext/additional-files/setup-cudatext.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-settingsDir="$(finddir B_USER_SETTINGS_DIRECTORY)/"
-dataDir="$(finddir B_SYSTEM_DATA_DIRECTORY)/cudatext/"
+settingsDir="$(finddir B_USER_SETTINGS_DIRECTORY)/cudatext"
+dataDir="$(finddir B_SYSTEM_DATA_DIRECTORY)/cudatext/settings_default"
 
 mkdir -p $settingsDir
 cp -rf "$dataDir" "$settingsDir"
