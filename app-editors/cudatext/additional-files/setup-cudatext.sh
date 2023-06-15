71dabafcefa6759bc5f11b4b5d236a53e9eb7cb3
Begasus <begasus@gmail.com>
2023-06-15 09:18:58 +0200

    cudatext, bump srcGitRev, enable 64bit

--- app-editors/cudatext/additional-files/setup-cudatext.sh
+++ app-editors/cudatext/additional-files/setup-cudatext.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-settingsDir="$(finddir B_USER_SETTINGS_DIRECTORY)/cudatext"
-dataDir="$(finddir B_SYSTEM_DATA_DIRECTORY)/cudatext/settings_default"
+settingsDir="$(finddir B_SYSTEM_DATA_DIRECTORY)/cudatext/settings_default"
+installSettingsDir="$(finddir B_USER_SETTINGS_DIRECTORY)/cudatext"
+dataDir="$(finddir B_SYSTEM_DATA_DIRECTORY)/cudatext/data"
+installDataDir="$(finddir B_USER_NONPACKAGED_DATA_DIRECTORY)/cudatext"
+pyDir="$(finddir B_SYSTEM_DATA_DIRECTORY)/cudatext/py"
 
-mkdir -p $settingsDir
-cp -rf "$dataDir" "$settingsDir"
+mkdir -p $installSettingsDir $installDataDir
+cp -rf "$settingsDir" "$installSettingsDir"
+cp -rf "$dataDir" "$installDataDir"
+cp -rf "$pyDir" "$installDataDir"
