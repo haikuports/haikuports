59edeb5c550309299ee1f55b76ba1f81b0e60086
Luca D'Amico <damico.luca91@live.it>
2023-09-19 20:04:29 +0200

    Another World (NEO-RAW): applied proposed changes (2nd patchset)

--- games-action/another_world/additional-files/another_world.sh
+++ games-action/another_world/additional-files/another_world.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+REQ_FILES=('BANK01' 'BANK02' 'BANK03' 'BANK04' 'BANK05' 'BANK06' 'BANK07'
+ 'BANK08' 'BANK09' 'BANK0A' 'BANK0B' 'BANK0C' 'BANK0D' 'MEMLIST.BIN')
 DATA_PATH=`findpaths -e B_FIND_PATH_DATA_DIRECTORY another_world | head -n 1`
 SAVE_PATH=`/bin/finddir B_USER_NONPACKAGED_DATA_DIRECTORY`/another_world/save
 
@@ -12,25 +14,22 @@ fi
 mkdir -p $SAVE_PATH # create save directory if it does not exist
 
 # Check if all files are present
-if [ ! -f $DATA_PATH/BANK01 ] \
-|| [ ! -f $DATA_PATH/BANK02 ] \
-|| [ ! -f $DATA_PATH/BANK03 ] \
-|| [ ! -f $DATA_PATH/BANK04 ] \
-|| [ ! -f $DATA_PATH/BANK05 ] \
-|| [ ! -f $DATA_PATH/BANK06 ] \
-|| [ ! -f $DATA_PATH/BANK07 ] \
-|| [ ! -f $DATA_PATH/BANK08 ] \
-|| [ ! -f $DATA_PATH/BANK09 ] \
-|| [ ! -f $DATA_PATH/BANK0A ] \
-|| [ ! -f $DATA_PATH/BANK0B ] \
-|| [ ! -f $DATA_PATH/BANK0C ] \
-|| [ ! -f $DATA_PATH/BANK0D ] \
-|| [ ! -f $DATA_PATH/MEMLIST.BIN ];
-then
-    if [ "$(alert --warning "Please copy original 'Another World' files (DOS version) to $DATA_PATH (BANK* and MEMLIST.BIN)." "Open data folder" "Cancel")" == "Open data folder" ];
+for file in ${REQ_FILES[@]};
+do
+    if [ ! -f $DATA_PATH/$file ];
     then
-        open $DATA_PATH
+        if [ $TERM == "dumb" ];
+        then
+            if [ "$(alert --warning "Missing file: $file. Please copy original 'Another World' files (DOS version) to $DATA_PATH (BANK* and MEMLIST.BIN)." "Open data folder" "Cancel")" == "Open data folder" ];
+            then
+                open $DATA_PATH
+            fi
+        else
+            echo "Missing file: $file. Please copy original 'Another World' files (DOS version) to $DATA_PATH (BANK* and MEMLIST.BIN)."
+        fi
+        exit
     fi
-else # all files are present, launch the game
-   neo-raw --datapath=$DATA_PATH --savepath=$SAVE_PATH $@
-fi
+done
+
+# all files are present, launch the game
+neo-raw --datapath=$DATA_PATH --savepath=$SAVE_PATH $@
