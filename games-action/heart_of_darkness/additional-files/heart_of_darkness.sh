a34804f0d81f1846c2a5b8ddbbe470e695976a4d
Jérôme Duval <jerome.duval@gmail.com>
2023-09-21 20:08:26 +0200

    heart_of_darkness.sh: rename data dir

--- games-action/heart_of_darkness/additional-files/heart_of_darkness.sh
+++ games-action/heart_of_darkness/additional-files/heart_of_darkness.sh
@@ -7,34 +7,30 @@ REQ_FILES=('Dark_hod.lvl' 'Dark_hod.mst' 'Dark_hod.sss' 'Fort_hod.lvl'
  'Pwr1_hod.lvl' 'Pwr1_hod.mst' 'Pwr1_hod.sss' 'Pwr2_hod.lvl' 'Pwr2_hod.mst' 
  'Pwr2_hod.sss' 'Rock_hod.lvl' 'Rock_hod.mst' 'Rock_hod.sss' 'paf/HOD.PAF'
  'setup/Setup.dat')
-DATA_PATH=`findpaths -e B_FIND_PATH_DATA_DIRECTORY hode | head -n 1`
-SAVE_PATH=`/bin/finddir B_USER_NONPACKAGED_DATA_DIRECTORY`/hode/save
+DATA_PATH=`findpaths -e B_FIND_PATH_DATA_DIRECTORY heart-of-darkness | head -n 1`
+SAVE_PATH=`/bin/finddir B_USER_NONPACKAGED_DATA_DIRECTORY`/heart-of-darkness/save
 
-if [ -z $DATA_PATH ];
-then
-	DATA_PATH=`/bin/finddir B_USER_NONPACKAGED_DATA_DIRECTORY`/hode
+if [ -z $DATA_PATH ]; then
+	DATA_PATH=`/bin/finddir B_USER_NONPACKAGED_DATA_DIRECTORY`/heart-of-darkness
 	mkdir -p $DATA_PATH # create data directory if it does not exist
 fi
 
 mkdir -p $SAVE_PATH # create save directory if it does not exist
 
 # Check if all files are present
-for file in ${REQ_FILES[@]};
-do
-    if [ ! -f $DATA_PATH/$file ];
-    then
-        if [ $TERM == "dumb" ];
-        then
-            if [ "$(alert --warning "Missing file: $file. Please copy original 'Heart of Darkness' files to $DATA_PATH." "Open data folder" "Cancel")" == "Open data folder" ];
-            then
+for file in ${REQ_FILES[@]}; do
+    if [ ! -f $DATA_PATH/$file ]; then
+        if [ $TERM == "dumb" ]; then
+            if [ "$(alert --warning "Missing file: $file. Please copy original 'Heart of Darkness' \
+files to $DATA_PATH." "Open data folder" "Cancel")" == "Open data folder" ]; then
                 open $DATA_PATH
             fi
         else
             echo "Missing file: $file. Please copy original 'Heart of Darkness' files to $DATA_PATH."
         fi
-        exit
+        exit 1
     fi
 done
 
 # all files are present, launch the game
-hode --datapath=$DATA_PATH --savepath=$SAVE_PATH $@
+"/system/apps/Heart of Darkness" --datapath=$DATA_PATH --savepath=$SAVE_PATH $@ &
