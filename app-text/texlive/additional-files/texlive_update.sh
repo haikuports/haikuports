ddb90a1487d04375cefb4be98be338277a6bd1de
Joachim Mairböck <j.mairboeck@gmail.com>
2024-03-27 18:46:49 +0100

    texlive: update to 2024
     - add a second (disabled) command to texlive_update.sh for ConTeXt MkIV
     - remove the now unneeded patching of the xetex post-install script
     - require the same version of texlive_core explicitly (via libkpathseaVersion)

--- app-text/texlive/additional-files/texlive_update.sh
+++ app-text/texlive/additional-files/texlive_update.sh
@@ -9,6 +9,9 @@ fmtutil-sys --all
 #notify --group "TeXLive" --title "Installation in progress" --messageID texliveInstallID --timeout 10000 "Updating TeXLive ConTeXt formats…"
 #mtxrun --generate
 
+#notify --group "TeXLive" --title "Installation in progress" --messageID texliveInstallID --timeout 10000 "Updating TeXLive LuaTeX ConTeXt formats…"
+#mtxrun --luatex --generate
+
 notify --group "TeXLive" --title "Installation in progress" --messageID texliveInstallID --timeout 10000 "Updating TeXLive font maps…"
 updmap-sys
 
