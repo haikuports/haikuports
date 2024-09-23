7f2be2e3fb868e697f2bc9dc699ee61e74fe255a
Peppersawce <157759066+Peppersawce@users.noreply.github.com>
2024-09-23 18:28:49 +0200

    Bunch o' fixes
    - Fix to cache directory issue
    - Recipe more compatible with 32 bit builds (DBIN_DIR)
    - Switched icon to official svg one

--- games-engines/gemrb/additional-files/gemrb-postinstall.sh
+++ games-engines/gemrb/additional-files/gemrb-postinstall.sh
@@ -8,12 +8,12 @@
 	#eventually read dir paths from the cfg
 #fi
 
-#CACHE_PATH=$(finddir B_USER_CACHE_DIRECTORY)/gemrb
+CACHE_PATH=$(finddir B_USER_CACHE_DIRECTORY)/gemrb
 SAVE_PATH=$(finddir B_USER_SETTINGS_DIRECTORY)/gemrb
 
-#if [ ! -e $CACHE_PATH ]; then
-#	mkdir -p $CACHE_PATH
-#fi
+if [ ! -e $CACHE_PATH ]; then
+	mkdir -p $CACHE_PATH
+fi
 if [ ! -e $SAVE_PATH ]; then
 	mkdir -p $SAVE_PATH
 fi
