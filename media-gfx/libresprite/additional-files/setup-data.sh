3e70dd890fe12d1f747162e39e4f5d37471e6ce1
Schrijvers Luc <begasus@gmail.com>
2024-10-18 14:02:50 +0200

    libresprite, revbump, cleanup/fix launching (#11223)

--- media-gfx/libresprite/additional-files/setup-data.sh
+++ media-gfx/libresprite/additional-files/setup-data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mkdir -r /boot/home/config/settings/libresprite/data
-cp -r /boot/system/data/libresprite/* /boot/home/config/settings/libresprite/data/
+mkdir -r /boot/home/config/settings/libresprite
+cp -r /boot/system/data/libresprite/data /boot/home/config/settings/libresprite
