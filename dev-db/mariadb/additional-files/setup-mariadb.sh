463a73abfb3a34264e77a86f07cd4000994bc3cb
DigitalBox98 <digitalbox098@gmail.com>
2025-02-23 22:48:53 +0000

    Notify user after system tables creation

--- dev-db/mariadb/additional-files/setup-mariadb.sh
+++ dev-db/mariadb/additional-files/setup-mariadb.sh
@@ -3,3 +3,5 @@
 # Create the mariadb system tables 
 /system/apps/MariaDB/scripts/mariadb-install-db --user=user --datadir=/boot/home/config/var/mariadb/data/ --auth-root-authentication-method=normal
 
+# Notify the user 
+notify --icon /system/kernel_`uname -p` "MariaDB system tables created"
