8d5b999d5a83ec6d289ce66621ef7b66021ec63b
Peppersawce <157759066+Peppersawce@users.noreply.github.com>
2025-04-08 09:31:02 +0200

    Recipe tweaks (#12104)

--- games-engines/gemrb/additional-files/gemrb-postinstall.sh
+++ games-engines/gemrb/additional-files/gemrb-postinstall.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#SYS_SETTINGS_PATH=$(finddir B_SYSTEM_SETTINGS_DIRECTORY)/gemrb
-
-#if [ ! -e $SYS_SETTINGS_PATH/GemRB.cfg ]; then
-	#move standard dir path declaration here
-#else
-	#eventually read dir paths from the cfg
-#fi
-
 CACHE_PATH=$(finddir B_USER_CACHE_DIRECTORY)/gemrb
 SAVE_PATH=$(finddir B_USER_SETTINGS_DIRECTORY)/gemrb
 
