f92d07b0b27796c61e961efa7942392bfa97324d
Peppersawce <michaelpeppers89@yahoo.it>
2025-04-17 04:56:05 +0200

    DevilutionX: make script check for spawn.mpq Add documentation as well Rework recipe, change settings path

--- games-rpg/devilutionx/additional-files/devilutionx.sh
+++ games-rpg/devilutionx/additional-files/devilutionx.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # Global variables
-PROGRAM_NAME="diasurgical/devilution"
-DATA_PATH=`/bin/finddir B_USER_SETTINGS_DIRECTORY`/$PROGRAM_NAME
+OLD_PROG_NAME="diasurgical/devilution"
+PROGRAM_NAME="devilutionx"
+OLD_DATA_PATH=`finddir B_USER_SETTINGS_DIRECTORY`/$OLD_PROG_NAME
+DATA_PATH=`finddir B_USER_SETTINGS_DIRECTORY`/$PROGRAM_NAME
 FILE=$DATA_PATH/diabdat.mpq
+DEMOFILE=$DATA_PATH/spawn.mpq
+
+# Migration if needed
+if [ -e $OLD_DATA_PATH ] && [ ! -e $DATA_PATH ]; then
+mkdir -p $DATA_PATH
+mv -t $DATA_PATH $OLD_DATA_PATH/*
+rm -rf `finddir B_USER_SETTINGS_DIRECTORY`/diasurgical
+fi
 
 # If the file exists...
-if [ -f $FILE ];
-then
-   # ...then start the program
-   `/bin/finddir B_SYSTEM_APPS_DIRECTORY`/DevilutionX/devilutionx
+if [ -f $FILE ] || [ -f $DEMOFILE ]; then
+# ...then start the program
+DevilutionX
 else
-
 # but if it isn't exist drop a warning
-if [ "$(alert --warning "Please, copy your original Diadblo 1 \"diabdat.mpq\" file to the Data Folder and rename it to lower-case." "Open Data Folder" "Cancel")" == "Open Data Folder" ]; then
-
+if [ "$(alert --warning "Please, copy Diablo's \"diabdat.mpq\" file or the demo's \"spawn.mpq\" to the Data Folder and rename it to lower-case." "Open Data Folder" "Cancel")" == "Open Data Folder" ]; then
 # then let's create the data Dir and open it
 mkdir -p $DATA_PATH
 open $DATA_PATH
