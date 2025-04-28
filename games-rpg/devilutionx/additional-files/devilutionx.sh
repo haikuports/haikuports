1400163f5da10f68bade865b1a5fd528ac3b5d43
Peppersawce <michaelpeppers89@yahoo.it>
2025-04-28 16:49:51 +0200

    DevilutionX: make script work with spawn.mpq Add documentation, change settings path, pack assets * Add Diablo shareware package

--- games-rpg/devilutionx/additional-files/devilutionx.sh
+++ games-rpg/devilutionx/additional-files/devilutionx.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
 # Global variables
-PROGRAM_NAME="diasurgical/devilution"
-DATA_PATH=`/bin/finddir B_USER_SETTINGS_DIRECTORY`/$PROGRAM_NAME
+OLD_DATA_PATH=`finddir B_USER_SETTINGS_DIRECTORY`/diasurgical/devilution
+DATA_PATH=`finddir B_USER_SETTINGS_DIRECTORY`/devilutionx
 FILE=$DATA_PATH/diabdat.mpq
+DEMOFILE=$DATA_PATH/spawn.mpq
+SHAREFILE="$(finddir B_SYSTEM_APPS_DIRECTORY)/devilutionx/spawn.mpq"
+
+# Migration if needed
+if [ -e $OLD_DATA_PATH ] && [ ! -e $DATA_PATH ]; then
+	mkdir -p $DATA_PATH
+	mv -t $DATA_PATH $OLD_DATA_PATH/*
+	rm -rf `finddir B_USER_SETTINGS_DIRECTORY`/diasurgical
+fi
+
+# Check for Shareware package
+if [ ! -e $SHAREFILE ]; then
+	SHAREFILE="$(finddir B_USER_APPS_DIRECTORY)/devilutionx/spawn.mpq"
+fi
+
+# DevilutionX has issues creating the path on its own
+if [ ! -e $DATA_PATH ]; then
+	mkdir -p $DATA_PATH
+fi
 
 # If the file exists...
-if [ -f $FILE ];
-then
-   # ...then start the program
-   `/bin/finddir B_SYSTEM_APPS_DIRECTORY`/DevilutionX/devilutionx
+if [ -f $FILE ] || [ -f $DEMOFILE ] || [ -f $SHAREFILE ]; then
+	# ...then start the program
+	DevilutionX $@
 else
-
-# but if it isn't exist drop a warning
-if [ "$(alert --warning "Please, copy your original Diadblo 1 \"diabdat.mpq\" file to the Data Folder and rename it to lower-case." "Open Data Folder" "Cancel")" == "Open Data Folder" ]; then
-
-# then let's create the data Dir and open it
-mkdir -p $DATA_PATH
-open $DATA_PATH
-fi
+	# but if it doesn't exist drop a warning
+	if [ "$(alert --warning "Please copy Diablo's \"diabdat.mpq\" file or the demo's \"spawn.mpq\" to the Data Folder and rename it to lower-case." "Open Data Folder" "Cancel")" == "Open Data Folder" ]; then
+		# then let's open the data Dir
+		open $DATA_PATH
+	fi
 fi
