4fa9e18f9a3b089eccd16f729c1c204ccbdf06d4
Gerasim Troeglazov <3dEyes@gmail.com>
2025-06-11 21:44:31 +1000

    orcaslicer: fix crash on start

--- media-gfx/orcaslicer/additional-files/OrcaSlicer.sh
+++ media-gfx/orcaslicer/additional-files/OrcaSlicer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 APPDIR="$(dirname -- "$(readlink -f -- "${0}")" )"
 cd "$APPDIR"
+export XDG_RUNTIME_DIR=/var/shared_memory
 LD_PRELOAD=$APPDIR/bin/lib/libfakelocaltime.so $APPDIR/bin/OrcaSlicer &
