60476c25f78feee84f9e52af0be24497b06a98c4
Peppersawce <michaelpeppers89@yahoo.it>
2025-11-13 22:44:16 +0100

    Update Omnispeak, better Keen4 startup script

--- games-action/omnispeak_keen4/additional-files/omni-keen4.sh
+++ games-action/omnispeak_keen4/additional-files/omni-keen4.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-omnibin=$(finddir B_SYSTEM_BIN_DIRECTORY)/omnispeak
 k4data=$(finddir B_SYSTEM_DATA_DIRECTORY)/omnispeak/keen4
 
-$omnibin /GAMEPATH $k4data
+if [ ! -e $k4data ]; then
+	k4data=$(finddir B_USER_DATA_DIRECTORY)/omnispeak/keen4
+	if [ ! -e $k4data ]; then
+		alert --stop "Game data not found!"
+		exit 1
+	fi
+fi
+
+omnispeak /GAMEPATH $k4data $@
