c09fcac6052fc6654d0eb1927d7692218f687c4e
Joachim Mairböck <j.mairboeck@gmail.com>
2025-11-15 20:04:49 +0100

    texlive: some bug fix updates
     * In texlive_update.sh, run mktexlsr at the end to hopefully not rebuild the
       format database all the time.
     * Add some patches to the lua config files to fix the paths for ConTeXt
       (SELFAUTO* variables in mtxrun.lua similar to kpathsea and texmfcnf.lua).

--- app-text/texlive/additional-files/texlive_update.sh
+++ app-text/texlive/additional-files/texlive_update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-notify --group "TeXLive" --title "Installation in progress" --messageID texliveInstallID --timeout 10000 "Updating TeXLive filename database…"
-mktexlsr
-
 notify --group "TeXLive" --title "Installation in progress" --messageID texliveInstallID --timeout 10000 "Updating TeXLive format files…"
 fmtutil-sys --all
 
@@ -14,5 +11,8 @@ fmtutil-sys --all
 
 notify --group "TeXLive" --title "Installation in progress" --messageID texliveInstallID --timeout 10000 "Updating TeXLive font maps…"
 updmap-sys
+
+notify --group "TeXLive" --title "Installation in progress" --messageID texliveInstallID --timeout 10000 "Updating TeXLive filename database…"
+mktexlsr
 
 notify --group "TeXLive" --title "Installation finished" --messageID texliveInstallID  "Took long enough…!"
